65e0792940d3052f44ba3fbe7484e4ef5378510d
Владимир Третьяков <vitretyakov@sbercloud.ru>
2022-02-14 23:38:22 +0300

    sprint-1 increment-1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,50 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"io"
+	"net/http"
+)
+
+var DB map[string]string
+
+func URLHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		strID := r.URL.Path
+		fmt.Println(strID[1:])
+		id := strID[1:]
+		if _, ok := DB[id]; !ok {
+			http.Error(w, "id not found", http.StatusBadRequest)
+			return
+		}
+		//w.Header().Set("content-type", "text/plain")
+		w.Header().Set("Location", DB[id])
+		w.WriteHeader(http.StatusTemporaryRedirect)
+		//w.WriteHeader(http.StatusOK)
+		//w.Write([]byte(DB[id]))
+	case http.MethodPost:
+		b, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fmt.Println(string(b))
+		id := uuid.New().String()[:8]
+		fmt.Println(id)
+		DB[id] = string(b)
+		w.Header().Set("content-type", "text/plain")
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(id))
+	default:
+		http.Error(w, "method not found", http.StatusBadRequest)
+		return
+	}
+}
+
+func main() {
+	DB = make(map[string]string)
+	http.HandleFunc("/", URLHandler)
+	http.ListenAndServe("localhost:8080", nil)
+}
