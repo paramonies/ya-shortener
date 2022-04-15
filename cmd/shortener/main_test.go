eea46f8754418a476942eb729c9fd39c75ff4f13
Shelamov <dmitrii.shelamov@softswiss.com>
2022-04-16 01:10:39 +0400

    fix

--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -8,6 +8,8 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/paramonies/internal/handlers"
+	"github.com/paramonies/internal/store"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -27,17 +29,17 @@ func TestMux(t *testing.T) {
 		want   want
 	}{
 		{
-			name:   "POST OK",
+			name:   "create short url from text/plain - OK",
 			body:   "https://practicum.yandex.ru",
 			method: http.MethodPost,
 			path:   "/",
 			want: want{
 				status: http.StatusCreated,
-				body:   fmt.Sprintf("http://localhost:8080/%d", hash("https://practicum.yandex.ru")),
+				body:   fmt.Sprintf("http://localhost:8080/%d", handlers.Hash("https://practicum.yandex.ru")),
 			},
 		},
 		{
-			name:   "POST invalid URL",
+			name:   "create short url from text/plain - invalid URL",
 			body:   "123",
 			method: http.MethodPost,
 			path:   "/",
@@ -46,16 +48,16 @@ func TestMux(t *testing.T) {
 			},
 		},
 		{
-			name:   "GET OK",
+			name:   "get original URL by short ID - OK",
 			method: http.MethodGet,
-			path:   fmt.Sprintf("/%d", hash("https://practicum.yandex.ru")),
+			path:   fmt.Sprintf("/%d", handlers.Hash("https://practicum.yandex.ru")),
 			want: want{
 				status:   http.StatusTemporaryRedirect,
 				location: "https://practicum.yandex.ru",
 			},
 		},
 		{
-			name:   "GET ID not found",
+			name:   "get original URL by short ID - not found",
 			method: http.MethodGet,
 			path:   fmt.Sprintf("/%s", "123"),
 			want: want{
@@ -63,9 +65,61 @@ func TestMux(t *testing.T) {
 				body:   "id not found\n",
 			},
 		},
+		{
+			name:   "create short URL from JSON - OK",
+			body:   `{"url":"https://practicum-1.yandex.ru"}`,
+			method: http.MethodPost,
+			path:   "/api/shorten",
+			want: want{
+				status: http.StatusCreated,
+				body:   `{"result":"http://localhost:8080/3003527198"}`,
+			},
+		},
+		{
+			name:   "create short URL from JSON - BAD REQUEST",
+			body:   `{"url":"https://practicum.yandex.ru"`,
+			method: http.MethodPost,
+			path:   "/api/shorten",
+			want: want{
+				status: http.StatusBadRequest,
+				body:   "unexpected end of JSON input\n",
+			},
+		},
+		{
+			name:   "ping",
+			method: http.MethodGet,
+			path:   "/ping",
+			want: want{
+				status: http.StatusOK,
+			},
+		},
+		{
+			name:   "get list URLs for userID - OK",
+			method: http.MethodGet,
+			path:   "/api/user/urls",
+			want: want{
+				status: http.StatusOK,
+				body:   `[{"short_url":"http://localhost:8080/3353207204","original_url":"https://practicum.yandex.ru"},{"short_url":"http://localhost:8080/3003527198","original_url":"https://practicum-1.yandex.ru"}]`,
+			},
+		},
+		{
+			name:   "create many short URLs from JSON - OK",
+			body:   `[{"correlation_id": "first","original_url": "https://practicum-2.yandex.ru"},{"correlation_id": "second","original_url": "https://practicum-3.yandex.ru"}]`,
+			method: http.MethodPost,
+			path:   "/api/shorten/batch",
+			want: want{
+				status: http.StatusCreated,
+				body:   `[{"correlation_id":"first","short_url":"http://localhost:8080/3159787651"},{"correlation_id":"second","short_url":"http://localhost:8080/740694524"}]`,
+			},
+		},
 	}
 
-	r := NewRouter()
+	cfg := Config{
+		SrvAddr: "localhost:8080",
+		BaseURL: "http://localhost:8080",
+	}
+
+	r := NewRouter(store.NewMapDB(), &cfg)
 	ts := httptest.NewServer(r)
 	defer ts.Close()
 
@@ -73,6 +127,12 @@ func TestMux(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			req, err := http.NewRequest(tt.method, ts.URL+tt.path, strings.NewReader(tt.body))
 			require.NoError(t, err)
+
+			cookie := &http.Cookie{
+				Name:  "user_id",
+				Value: "wSzPHUbHwQ/WKQ==",
+			}
+			req.AddCookie(cookie)
 
 			client := &http.Client{}
 			client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
