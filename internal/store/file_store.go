867bdde4e658a9e11de2eedecbb059774c146d3e
Владимир Третьяков <vitretyakov@sbercloud.ru>
2022-05-13 22:18:27 +0300

    move map and file stores in separate files

--- internal/store/file_store.go
+++ internal/store/file_store.go
@@ -7,54 +7,6 @@ import (
 	"os"
 )
 
-type MapDB struct {
-	DB map[string]map[string]string
-}
-
-func NewMapDB() *MapDB {
-	return &MapDB{
-		DB: make(map[string]map[string]string),
-	}
-}
-
-func (db *MapDB) Set(key, val, userID string) error {
-	db.DB[key] = map[string]string{
-		"url":    val,
-		"userID": userID,
-	}
-	return nil
-}
-
-func (db *MapDB) Get(key string) (string, error) {
-	val, ok := db.DB[key]
-	if !ok {
-		return "", fmt.Errorf("key %s not found in database", key)
-	}
-	return val["url"], nil
-}
-
-func (db *MapDB) GetAllByID(id string) (map[string]string, error) {
-	data := make(map[string]string)
-	for key, row := range db.DB {
-		if row["userID"] == id {
-			data[key] = row["url"]
-		}
-	}
-	return data, nil
-}
-
-func (db *MapDB) Delete(urlID, userID string) error {
-	return nil
-}
-
-func (db *MapDB) Ping() error {
-	return nil
-}
-
-func (db *MapDB) Close() error {
-	return nil
-}
-
 type Record struct {
 	ID     string `json:"id"`
 	URL    string `json:"url"`
